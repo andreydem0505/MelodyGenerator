9d6876bcab53f567488252db6143403dd032a815
Artem Sumin <qpeyba@ya.ru>
2026-04-08 16:46:48 +0300

    organize logic for the args parsing

---
+++ launch_options.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def launch_options():
+    parser = argparse.ArgumentParser(description='Generate MIDIolody')
+    parser.add_argument('--chords', type=int, default=16, help='Number of chords (default: 16)')
+    parser.add_argument('--tempo', type=float, default=200.0, help='Tempo in BPM (default: 200.0)')
+    parser.add_argument('--octave', type=int, default=5, help='Octave number (default: 5)')
+    parser.add_argument('--minor-chance', type=float, default=0.4, help='Chance of minor chords (default: 0.4)')
+    parser.add_argument('--tonic-chance', type=float, default=0.2, help='Initial chance of tonic chord (default: 0.2)')
+    parser.add_argument('--final-tonic', action='store_true', help='Guarantees that the tonic is placed on the final beat (add if needed)')
+    parser.add_argument('--time-sig', type=str, default='3/4', help='Time signature (default: 3/4)')
+    parser.add_argument('--output', '-o', type=str, default='result.mid', help='Output MIDI file (default: result.mid)')
+
+    return parser.parse_args()
