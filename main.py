b9f74f62d0df809bf939dce047e0d87a1c7a249e
Artem Sumin <qpeyba@ya.ru>
2026-04-08 15:33:59 +0300

    this argument should not be changed by the user

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import mido
 from chords import compose_chords_sequence
 from notes import get_chords_notes, Note
 
+TICKS_PER_BEAT = 96
 
-parser = argparse.ArgumentParser(description='Generate melody MIDI file')
+parser = argparse.ArgumentParser(description='Generate MIDIolody')
 parser.add_argument('--chords', type=int, default=16, help='Number of chords (default: 16)')
 parser.add_argument('--tempo', type=float, default=200.0, help='Tempo in BPM (default: 200.0)')
 parser.add_argument('--octave', type=int, default=5, help='Octave number (default: 5)')
-parser.add_argument('--ticks', type=int, default=96, help='Ticks per beat (default: 96)')
 parser.add_argument('--minor-chance', type=float, default=0.4, help='Chance of minor chords (default: 0.4)')
 parser.add_argument('--tonic-chance', type=float, default=0.2, help='Initial chance of tonic chord (default: 0.2)')
 parser.add_argument('--final-tonic', action='store_true', help='Guarantee tonic in the last beat')
 parser.add_argument('--time-sig', type=str, default='3/4', help='Time signature (default: 3/4)')
-parser.add_argument('--output', type=str, default='result.mid', help='Output MIDI file (default: result.mid)')
+parser.add_argument('--output', '-o', type=str, default='result.mid', help='Output MIDI file (default: result.mid)')
 
 args = parser.parse_args()
 
@@ -25,7 +25,7 @@ print(result_chords)
 keys: list[Note] = get_chords_notes(result_chords, args.octave, time_sig)
 
 
-mid = mido.MidiFile(type=0, ticks_per_beat=args.ticks)
+mid = mido.MidiFile(type=0, ticks_per_beat=TICKS_PER_BEAT)
 track = mido.MidiTrack()
 mid.tracks.append(track)
 
