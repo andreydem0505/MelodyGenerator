526b7f884eeb4295df7a7a6d143c86dc26b6bffb
Artem Sumin <qpeyba@ya.ru>
2026-04-08 18:49:46 +0300

    refactor MIDI saving

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import mido
-from launch_options import launch_options
+from launch_options import launch_options, parse_time_signature
 from chords import compose_chords_sequence
 from notes import get_chords_notes, Note
+from to_midi import save
 
 TICKS_PER_BEAT = 96
 
@@ -14,23 +14,5 @@ result_chords: list[int] = compose_chords_sequence(args.chords, minor_chance=arg
 print(result_chords)
 keys: list[Note] = get_chords_notes(result_chords, args.octave, time_sig)
 
+save(keys, args.tempo, args.output)
 
-mid = mido.MidiFile(type=0, ticks_per_beat=TICKS_PER_BEAT)
-track = mido.MidiTrack()
-mid.tracks.append(track)
-
-track.append(mido.MetaMessage('set_tempo', tempo=mido.bpm2tempo(args.tempo), time=0))
-
-events = []
-for note in keys:
-    events.append((note.position, 'note_on', note.key, note.velocity))
-    events.append((note.position + note.length, 'note_off', note.key, 0))
-
-events.sort(key=lambda e: (e[0], e[1] == 'note_on'))
-
-prev_time = 0
-for abs_time, msg_type, key, velocity in events:
-    track.append(mido.Message(msg_type, note=key, velocity=velocity, time=abs_time - prev_time))
-    prev_time = abs_time
-
-mid.save(args.output)
