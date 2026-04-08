8f620c0b3a7b33e0629ed225caabcc7b60c04a8d
qpeyba <145938039+qpeyba@users.noreply.github.com>
2026-04-08 14:49:32 +0300

    implement arg params

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
+import argparse
 import mido
 from chords import compose_chords_sequence
 from notes import get_chords_notes, Note
 
 
-CHORDS_NUMBER = 16
+parser = argparse.ArgumentParser(description='Generate melody MIDI file')
+parser.add_argument('--chords', type=int, default=16, help='Number of chords (default: 16)')
+parser.add_argument('--tempo', type=float, default=200.0, help='Tempo in BPM (default: 200.0)')
+parser.add_argument('--octave', type=int, default=5, help='Octave number (default: 5)')
+parser.add_argument('--ticks', type=int, default=96, help='Ticks per beat (default: 96)')
+parser.add_argument('--minor-chance', type=float, default=0.4, help='Chance of minor chords (default: 0.4)')
+parser.add_argument('--tonic-chance', type=float, default=0.2, help='Initial chance of tonic chord (default: 0.2)')
+parser.add_argument('--final-tonic', action='store_true', help='Guarantee tonic in the last beat')
+parser.add_argument('--time-sig', type=str, default='3/4', help='Time signature (default: 3/4)')
+parser.add_argument('--output', type=str, default='result.mid', help='Output MIDI file (default: result.mid)')
 
-TEMPO = 200.0
+args = parser.parse_args()
 
-OCTAVE_NUMBER = 5
+time_sig_parts = args.time_sig.split('/')
+time_sig = int(time_sig_parts[0]) / int(time_sig_parts[1])
 
-TICKS_PER_BEAT = 96
-
-result_chords: list[int] = compose_chords_sequence(CHORDS_NUMBER, minor_chance=0.4, tonic_chance_init=0.2, final_tonic=False)
+result_chords: list[int] = compose_chords_sequence(args.chords, minor_chance=args.minor_chance, tonic_chance_init=args.tonic_chance, final_tonic=args.final_tonic)
 print(result_chords)
-keys: list[Note] = get_chords_notes(result_chords, OCTAVE_NUMBER, 3/4)
+keys: list[Note] = get_chords_notes(result_chords, args.octave, time_sig)
 
 
-mid = mido.MidiFile(type=0, ticks_per_beat=TICKS_PER_BEAT)
+mid = mido.MidiFile(type=0, ticks_per_beat=args.ticks)
 track = mido.MidiTrack()
 mid.tracks.append(track)
 
-track.append(mido.MetaMessage('set_tempo', tempo=mido.bpm2tempo(TEMPO), time=0))
+track.append(mido.MetaMessage('set_tempo', tempo=mido.bpm2tempo(args.tempo), time=0))
 
 events = []
 for note in keys:
@@ -34,4 +43,4 @@ for abs_time, msg_type, key, velocity in events:
     track.append(mido.Message(msg_type, note=key, velocity=velocity, time=abs_time - prev_time))
     prev_time = abs_time
 
-mid.save("result.mid")
+mid.save(args.output)
