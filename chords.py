183fbf45d1d2ee9f599f027015f4ea82565c378a
Artem Sumin <qpeyba@ya.ru>
2026-05-10 23:01:49 +0300

    add binary choice of melody mode (major/minor)

--- chords.py
+++ chords.py
@@ -2,16 +2,16 @@ import random
 from notes_math import add, sub
 
 def compose_chords_sequence(chords_number: int,
+                            mode: str = 'minor',
                             tonic: int = random.randint(1, 12),
-                            minor_chance: float = 0.6,
                             tonic_chance_init: float = 0.17,
                             final_tonic: bool = False) -> list[int]:
+    if mode not in ['minor', 'major']:
+        raise ValueError("mode must be 'minor' or 'major'")
     if not 1 <= tonic <= 12:
         raise ValueError("tonic must be in range [1, 12]")
     if chords_number < 1:
         raise ValueError("chords_number must be >= 1")
-    if not 0.0 <= minor_chance <= 1.0:
-        raise ValueError("minor_chance must be in range [0.0, 1.0]")
     if not 0.0 <= tonic_chance_init <= 1.0:
         raise ValueError("tonic_chance_init must be in range [0.0, 1.0]")
 
@@ -26,10 +26,16 @@ def compose_chords_sequence(chords_number: int,
             result_sequence.append(tonic)
             tonic_chance = tonic_chance_init
         else:
-            if random.random() > minor_chance:
-                next_chord = random.choice(possible_chords[:2])
+            if mode == 'minor':
+                if random.random() < 0.75:
+                    next_chord = random.choice(possible_chords[-3:])
+                else:
+                    next_chord = random.choice(possible_chords[:2])
             else:
-                next_chord = random.choice(possible_chords[-3:])
+                if random.random() < 0.25:
+                    next_chord = random.choice(possible_chords[-3:])
+                else:
+                    next_chord = random.choice(possible_chords[:2])
             result_sequence.append(next_chord)
             tonic_chance += min(1.0 - tonic_chance, chance_step)
     if final_tonic: result_sequence.append(tonic)
